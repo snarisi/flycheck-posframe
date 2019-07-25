498beb7a9608d15ccdf83875632bdebd7c6fa191
Sam Narisi <snarisi@gmail.com>
2019-07-25 14:32:05 -0400

    Redirect frame focus

--- flycheck-posframe.el
+++ flycheck-posframe.el
@@ -201,6 +201,11 @@ Only the `foreground' is used in this face."
        :internal-border-width flycheck-posframe-border-width
        :internal-border-color (face-foreground'flycheck-posframe-border-face nil t)
        :poshandler poshandler))
+
+    (setq current-posframe-frame
+	(buffer-local-value 'posframe--frame (get-buffer flycheck-posframe-buffer)))
+    (redirect-frame-focus current-posframe-frame (frame-parent current-posframe-frame))
+
     (dolist (hook flycheck-posframe-hide-posframe-hooks)
       (add-hook hook #'flycheck-posframe-hide-posframe nil t))))
 
